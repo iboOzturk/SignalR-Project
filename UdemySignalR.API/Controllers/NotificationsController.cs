722b63c9e5aee7fa814636f72048298444c03860
iboOzturk <ozturk.ibo@outlook.com>
2023-12-07 17:50:38 +0300

    Add project files.

---
+++ UdemySignalR.API/Controllers/NotificationsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using UdemySignalR.API.Hubs;
+
+namespace UdemySignalR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public NotificationsController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            await _hubContext.Clients.All.
+                SendAsync("Notify", $"Arkadaşlar takımlar {teamCount} kişi olacak");
+            return Ok();
+
+        }
+    }
+}
---
+++ UdemySignalR.API/Hubs/MyHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace UdemySignalR.API.Hubs
+{
+    public class MyHub:Hub
+    {
+        private static List<string> Names { get; set; }=new List<string>();
+        private static int ClientCount { get; set; } = 0;
+
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName",name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
