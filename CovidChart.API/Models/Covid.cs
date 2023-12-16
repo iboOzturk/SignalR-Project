4fceaf56106b5f501a9c6c84c5461ca3c7c478a6
iboOzturk <ozturk.ibo@outlook.com>
2023-12-16 15:54:31 +0300

    Covid projesi için API projesi oluşturuldu.

---
+++ CovidChart.API/Models/Covid.cs
@@ -0,0 +1,19 @@
+﻿namespace CovidChart.API.Models
+{
+    public enum ECity
+    {
+        Istanbul=1,
+        Ankara=2,
+        Bursa=3,
+        Izmir=4,
+        Konya=5,
+        Antalya=6
+    }
+    public class Covid
+    {
+        public int Id { get; set; }
+        public ECity City { get; set; }
+        public int Count { get; set; }
+        public DateTime CovidDate { get; set; } 
+    }
+}
